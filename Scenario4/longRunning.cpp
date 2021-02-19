c18e840c3da83d9c0f6460b6eaca7b3adf65a770
Ahson Khan <ahkha@microsoft.com>
2021-02-19 11:11:38 -0800

    Add scenario 4 for LRO.

---
+++ Scenario4/longRunning.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string>
+
+// Add the necessary #include headers here
+
+// Run this application that is using the C++ Azure Storage SDK to copy a large blob and track progress by 
+// printing a timer, i.e. "not done <number of seconds since the operation started>" message to the console every second \
+// the copy operation is in progress.
+// Note: The necessary azure-storage-blobs dependency has already been installed for you.
+// Note: Here are the Azure Storage account details. The connection string to connect to the service is set in the environment variable.
+void CopyWithProgress()
+{
+
+}
+
+int main()
+{
+    CopyWithProgress();
+    puts ("Task 4 complete");
+    return 0;
+}
